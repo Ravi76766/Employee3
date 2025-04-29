54c04f44e1de9b9da86b92d4566fe70623d91d8a
Ravi76766 <raviu2687@gmail.com>
2025-04-29 10:03:17 +0530

    saved

--- Employe.java
+++ Employe.java
@@ -1,33 +1,33 @@
 public class Employe {
- String name;
- int id;
- double salary;
-
-
-
-
-
- public Employe(String name, int id, double salary,) 
- {
-    this.name = name;
-    this.id = id;
-    this.salary = salary;
-   
-
- }
- 
- public void display()
- {
-    System.out.println("Employee Name:"+name);
-    System.out.println("Employee ID:"+id);
-    System.out.println("Employee Salary:"+salary);
-   
-
- }
-
- public static void main(String[] args) {
-    Employe emp=new Employe("Ravi", 147,50000);
-    emp.display();
-}
-}
-
+        String name;
+        int id;
+        double salary;
+       
+       
+       
+       
+       
+        public Employe(String name, int id, double salary) 
+        {
+           this.name = name;
+           this.id = id;
+           this.salary = salary;
+          
+       
+        }
+        
+        public void display()
+        {
+           System.out.println("Employee Name:"+name);
+           System.out.println("Employee ID:"+id);
+           System.out.println("Employee Salary:"+salary);
+          
+       
+        }
+       
+        public static void main(String[] args) {
+           Employe emp=new Employe("Ravi", 147,50000);
+           emp.display();
+       }
+       }
+       
